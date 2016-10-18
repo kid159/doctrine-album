a0b50e6ea18db095736b8214e86c6e1a0db9ee1f
kid159 <kidtran159@gmail.com>
2016-10-19 00:38:48 +0700

    d2

---
+++ data/album.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 17 Octobre 2016 à 20:55
+-- Version du serveur :  5.6.16
+-- Version de PHP :  5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `album_doctrine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `album`
+--
+
+CREATE TABLE IF NOT EXISTS `album` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `artist` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `album`
+--
+
+INSERT INTO `album` (`id`, `artist`, `title`) VALUES
+(1, 'The  Military  Wives', 'In  My  Dreams 1'),
+(2, 'Adele', '21'),
+(3, 'Bruce  Springsteen', 'Wrecking Ball (Deluxe)'),
+(4, 'Lana  Del  Rey', 'Born  To  Die'),
+(5, 'Gotye', 'Making  Mirrors');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ data/global.php
@@ -0,0 +1,22 @@
+<?php
+// config/autoload/global.php:
+return array(
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'dbname'   => 'album_doctrine',
+                )
+            )
+        )
+    ),
+);
